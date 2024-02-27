7ccdea08bf3371bc374a5319f2d5e6172b26fb34
Austin Oler <oleraustin@gmail.com>
2024-02-27 11:33:59 -0500

    create apollo provider

--- Develop/client/src/App.jsx
+++ Develop/client/src/App.jsx
@@ -1,14 +1,21 @@
 import './App.css';
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 import { Outlet } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
 
+// Create a new ApolloClient instance
+const client = new ApolloClient({
+  uri: 'http://localhost:3001/graphql', // Set your GraphQL endpoint here
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
-    <>
+    <ApolloProvider client={client}> {/* Wrap your component tree with ApolloProvider */}
       <Navbar />
       <Outlet />
-    </>
+    </ApolloProvider>
   );
 }
 
